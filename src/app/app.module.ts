304deea9cff9596f8e90b0757bfb590f8ff6e93e
SatyanarayanaReddyMallidi <satya.mallidi6@gmail.com>
2023-02-08 21:49:15 +0530

    Basic SetUp

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { PaymentComponent } from './payment/payment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    OrderComponent,
+    PaymentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
